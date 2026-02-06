9bb82ce8a5a291629145f3db621a1ae021bbae4d
Alejo <20245281@esen.edu.sv>
2026-02-06 07:59:25 -0600

    StarRating

---
+++ Practica3/src/components/StarRating.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+const containerStyle = {
+    display: "flex",
+    alignItems: "center",
+    gap: "16px"
+};
+
+const starContainerStyle = {
+    display: "flex",
+    gap: "4px"
+};

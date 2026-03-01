73af90c3c1d0656520e6d7460db13fb6cbc04268
Alejo <20245281@esen.edu.sv>
2026-03-01 14:35:06 -0600

    extras

--- Guia4_20245281/src/components/BudgetTracker.jsx
+++ Guia4_20245281/src/components/BudgetTracker.jsx
@@ -1,6 +1,11 @@
-import { AmountDisplay } from "./AmountDisplay"
+import React, { useContext } from "react";
+import { AmountDisplay } from "./AmountDisplay";
+import { BudgetStateContext } from "../context/BudgetContext";
 
 export const BudgetTracker = () => {
+    const state = useContext(BudgetStateContext);
+    const totalExpenses = state.expenses.reduce((total, expense) => total + expense.amount, 0);
+    const remainingBudget = state.budget - totalExpenses;
     return (
         <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
             <div className="flex justify-center">
@@ -11,9 +16,9 @@ export const BudgetTracker = () => {
                     className="bg-pink-600 w-full p-2 text-white uppercase font-bold rounded-lg"
                 >
                     Resetear app</button>
-                <AmountDisplay amount={300} label="Presupuesto" />
-                <AmountDisplay amount={200} label="Disponible" />
-                <AmountDisplay amount={100} label="Gastado" />
+                <AmountDisplay amount={state.budget} label="Presupuesto" />
+                <AmountDisplay amount={remainingBudget} label="Disponible" />
+                <AmountDisplay amount={totalExpenses} label="Gastado" />
             </div>
         </div>
     )

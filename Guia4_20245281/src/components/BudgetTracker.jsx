3453e1df158685c20c787a9c405942bb83406e7b
Alejo <20245281@esen.edu.sv>
2026-03-01 15:08:36 -0600

    Porcentaje de gasto funcional

--- Guia4_20245281/src/components/BudgetTracker.jsx
+++ Guia4_20245281/src/components/BudgetTracker.jsx
@@ -1,15 +1,25 @@
 import React, { useContext } from "react";
 import { AmountDisplay } from "./AmountDisplay";
 import { BudgetStateContext } from "../context/BudgetContext";
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 
 export const BudgetTracker = () => {
     const state = useContext(BudgetStateContext);
     const totalExpenses = state.expenses.reduce((total, expense) => total + expense.amount, 0);
     const remainingBudget = state.budget - totalExpenses;
+    const percentage = ((totalExpenses / state.budget) * 100).toFixed(2);
     return (
         <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
             <div className="flex justify-center">
-                <img src="/grafico.jpg" alt=""/>
+                <CircularProgressbar
+                    value={percentage}
+                    text={`${percentage}%`}
+                    styles={buildStyles({
+                        pathColor: (percentage < 100) ? '#3b82f6' : '#dc2626',
+                        trailColor: '#F5F5F5'
+                    })}
+                />
             </div>
             <div className="flex flex-col justify-center items-center gap-8">
                 <button
